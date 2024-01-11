c4e48a29a87ddbc704b98183d84009180b9f0da1
Ivan B <sensenabi@gmail.com>
2024-01-11 16:04:06 -0500

    increase stale time

---
+++ src/app/api/route.ts
@@ -0,0 +1,35 @@
+import { type NextRequest } from 'next/server'
+
+export const runtime = "edge";
+
+export async function GET(request: NextRequest) {
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+  const title = await fetch('https://hacker-news.firebaseio.com/v0/newstories.json', {
+    next: {
+      revalidate: 600
+    }
+  })
+    .then(response => response.json())
+    .then(async storyIds => {
+      const randomIdx = Math.floor(Math.random() * storyIds.length);
+      const storyId = storyIds[randomIdx];
+      console.log({ storyId })
+      return fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json`)
+        .then(response => response.json())
+        .then(story => {
+          return (story.title) as string;
+        })
+    })
+  return new Response(
+    JSON.stringify({ title }), {
+    status: 200,
+    headers: {
+      // 'Cache-Control': 'public, max-age=0, s-maxage=60, stale-while-revalidate=59',
+      'Cache-Control': 'public, max-age=0, s-maxage=60, stale-while-revalidate=86400, stale-if-error=86400',
+      // 'CDN-Cache-Control': 'public, s-maxage=60',
+      // 'Vercel-CDN-Cache-Control': 'public, s-maxage=60',
+    }
+  })
+}
+
+
